4438080ec2420ffb721d15310afd67d723795be8
rpragathi <116181002+rpragathi@users.noreply.github.com>
2026-05-03 22:58:55 +0530

    rotate function
    Added direction parameter to rotate function for left and right rotations.

--- arrays/rotate_array_by_k.py
+++ arrays/rotate_array_by_k.py
@@ -1,19 +1,25 @@
-def rotate(arr, k):
-    """
-    Rotate array to the right by k steps
-    Time Complexity: O(n)
-    Space Complexity: O(1)
-    """
-
+def rotate(arr, k, direction):
     n = len(arr)
-    k = k % n
-    def reverse(start, end):
-        while start < end:
-            arr[start], arr[end] = arr[end], arr[start]
-            start += 1
-            end -= 1
-    reverse(0, n - 1)
-    reverse(0, k - 1)
-    reverse(k, n - 1)
+    k %= n
+
+    def rev(l, r):
+        while l < r:
+            arr[l], arr[r] = arr[r], arr[l]
+            l += 1
+            r -= 1
+
+    if direction == "right":
+        rev(0, n - 1)
+        rev(0, k - 1)
+        rev(k, n - 1)
+
+    elif direction == "left":
+        rev(0, k - 1)
+        rev(k, n - 1)
+        rev(0, n - 1)
+
     return arr
-print(rotate([1,2,3,4,5], 2))  
+
+
+print(rotate([2, 3, 4], 1, "left"))  
+print(rotate([2, 3, 4], 1, "right"))  
